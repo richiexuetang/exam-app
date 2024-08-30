8cbd9446e84e87b73fb5888c582d39e617e71f63
richiexue <richard772794659@gmail.com>
2024-08-30 12:36:32 -0700

    validate test results

--- components/form/question-form.tsx
+++ components/form/question-form.tsx
@@ -23,6 +23,7 @@ interface QuestionFormProps {
 }
 
 export const QuestionForm = ({ questions }: QuestionFormProps) => {
+  const [incorrectAnswers, setIncorrectAnswers] = React.useState<string[]>([])
   const initialValues = {} as any
 
   questions.map(
@@ -30,69 +31,100 @@ export const QuestionForm = ({ questions }: QuestionFormProps) => {
       (initialValues[`q${index + 1}`] = question.isMultiSelect ? [] : "")
   )
 
-  const form = useForm({
+  const form = useForm<typeof initialValues>({
     defaultValues: { ...initialValues },
   })
 
-  const onSubmit = (values: any) => {
-    console.log(values)
+  const onSubmit = (values: typeof initialValues) => {
+    setIncorrectAnswers([])
+
+    for (const [key, _] of Object.entries(values)) {
+      const questionNumber = parseInt(key.slice(1))
+      const question = questions[questionNumber - 1]
+      const userAnswer = values[key]
+
+      if (question.isMultiSelect) {
+        if (userAnswer.length != 2) {
+          setIncorrectAnswers((prev) => [...prev, key])
+        } else {
+          for (var i = 0; i < question.options.length; i++) {
+            const option = question.options[i]
+            if (option.isAnswer && !userAnswer.includes(option.value)) {
+              setIncorrectAnswers((prev) => [...prev, key])
+              break
+            }
+          }
+        }
+      } else {
+        const correctAnswer = question.options.find(
+          (option) => option.isAnswer
+        )?.value
+        const isMatch = userAnswer === correctAnswer
+        if (!isMatch) {
+          setIncorrectAnswers((prev) => [...prev, key])
+        }
+      }
+    }
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        {questions?.map((question, index) => {
-          if (question.isMultiSelect) {
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          {questions?.map((question, index) => {
+            if (question.isMultiSelect) {
+              return (
+                <MultiSelectQuestion
+                  key={question.prompt}
+                  form={form}
+                  question={question}
+                  index={index}
+                />
+              )
+            }
             return (
-              <MultiSelectQuestion
+              <FormField
+                control={form.control}
                 key={question.prompt}
-                form={form}
-                question={question}
-                index={index}
+                // @ts-ignore
+                name={`q${(index + 1).toString()}`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      {index + 1}. {question.prompt}
+                    </FormLabel>
+                    <FormControl>
+                      <RadioGroup
+                        onValueChange={field.onChange}
+                        defaultValue={field.value as string}
+                        className="flex flex-col space-y-1"
+                      >
+                        {question.options.map((option, index) => (
+                          <FormItem
+                            className="flex items-center space-x-3 space-y-0"
+                            key={`${option.value}${index}`}
+                          >
+                            <FormControl>
+                              <RadioGroupItem
+                                value={option.value}
+                                id={option.value}
+                              />
+                            </FormControl>
+                            <Label htmlFor={option.value}>{option.value}</Label>
+                          </FormItem>
+                        ))}
+                      </RadioGroup>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
             )
-          }
-          return (
-            <FormField
-              control={form.control}
-              key={question.prompt}
-              // @ts-ignore
-              name={`q${(index + 1).toString()}`}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>
-                    {index + 1}. {question.prompt}
-                  </FormLabel>
-                  <FormControl>
-                    <RadioGroup
-                      onValueChange={field.onChange}
-                      defaultValue={field.value as string}
-                      className="flex flex-col space-y-1"
-                    >
-                      {question.options.map((option, index) => (
-                        <FormItem
-                          className="flex items-center space-x-3 space-y-0"
-                          key={`${option.value}${index}`}
-                        >
-                          <FormControl>
-                            <RadioGroupItem
-                              value={option.value}
-                              id={option.value}
-                            />
-                          </FormControl>
-                          <Label htmlFor={option.value}>{option.value}</Label>
-                        </FormItem>
-                      ))}
-                    </RadioGroup>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          )
-        })}
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+          })}
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+      {incorrectAnswers.length > 0 && <div>{incorrectAnswers.length}/50</div>}
+    </>
   )
 }
